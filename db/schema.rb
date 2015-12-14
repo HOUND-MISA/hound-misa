c191f71879a84d02f5fe1d5a3fa715866b0921ff
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2015-12-14 12:24:17 +0800

    with come styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213192801) do
+ActiveRecord::Schema.define(version: 20151213183636) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
