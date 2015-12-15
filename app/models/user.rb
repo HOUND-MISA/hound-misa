56ba2ac4c6b27725fb658a54d55ab634f221eddd
jacqhernandez <jm.hernandez96@yahoo.com>
2015-12-16 01:39:54 +0800

    get fb name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
-        #user.email = auth.info.email
-        user.first_name = auth.info.email
+        user.email = auth.info.email
+        user.first_name = auth.info.first_name
+        user.last_name = auth.info.last_name
         user.password = Devise.friendly_token[0,20]
       end
   end
