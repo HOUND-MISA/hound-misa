a660591ed2b59957bfe49916bf65be20b4797de4
demijade <sy_judy@yahoo.com>
2015-12-17 22:18:28 +0800

    added upload image in edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214070352) do
+ActiveRecord::Schema.define(version: 20151217134518) do
 
   create_table "event_attendees", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -85,6 +85,10 @@ ActiveRecord::Schema.define(version: 20151214070352) do
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
